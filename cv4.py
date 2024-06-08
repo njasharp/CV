be0a75722b1ad2470ea4a549eac9ccf86958866e
Darryl Williams <39777038+njasharp@users.noreply.github.com>
2024-06-08 13:06:24 -0700

    Add files via upload

--- cv4.py
+++ cv4.py
@@ -18,8 +18,9 @@ if uploaded_file is not None:
         # Convert the image to grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Load the face cascade
-        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + haarcascade_frontalface_default.xml)
+        # Provide the full path to the haarcascade XML file
+        cascades_dir = './'
+        face_cascade = cv2.CascadeClassifier(cascades_dir + 'haarcascade_frontalface_default.xml')
 
         # Detect faces in the image
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
